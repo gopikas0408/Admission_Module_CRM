366b60bbf3f11209354475c5ec6e9d942f5f1cba
gopikas0408 <gopikas04082005@gmail.com>
2026-06-10 12:56:01 +0530

    Auto create superuser on render

--- csc_crm/wsgi.py
+++ csc_crm/wsgi.py
@@ -14,3 +14,20 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'csc_crm.settings')
 
 application = get_wsgi_application()
+
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if username and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
